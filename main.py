4b28e91c251ceeaa7362a5e93021eecb77a4e603
Samokhin Alexander <icorwin74@gmail.com>
2023-03-22 15:39:40 +0300

    _save #2

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import aiohttp
 import asyncio
+import ssl
+import aiohttp
+import certifi
 
 
-async def fetch(session, url):
-    async with session.get(url) as response:
+async def fetch(session, url, ssl_context):
+    async with session.get(url, ssl_context=ssl_context) as response:
         response.raise_for_status()
         return await response.text()
 
 
 async def main():
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://example.com')
+        html = await fetch(
+            session,
+            'http://inosmi.ru/20230322/kitay-261582482.html',
+            ssl_context=ssl_context
+        )
         print(html)
 
 
