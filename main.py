7c13a61bfee21834f3484a707f39ab59a6cb543c
Samokhin Alexander <icorwin74@gmail.com>
2023-03-24 13:10:33 +0300

    Add handling exceptions

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import asyncio
 import ssl
+from enum import Enum
 import aiohttp
 import aiofiles
 from anyio import create_task_group
+from async_timeout import timeout
 import certifi
 import pymorphy2
 from adapters.inosmi_ru import sanitize
+from adapters.exceptions import ArticleNotFound
 from text_tools import split_by_words, calculate_jaundice_rate
 
 TEST_ARTICLES = [
@@ -14,7 +17,19 @@ TEST_ARTICLES = [
     'https://inosmi.ru/20230323/konflikt-261628210.html',
     'https://inosmi.ru/20230323/lavra-261621781.html',
     'https://inosmi.ru/20230323/ssha-261613436.html',
+    'https://inosmi.ru/not/exist.html',
+    'https://pikabu.ru/story/reklama_mvd_poka_ne_udalili_10071263',
     ]
+
+
+class ProcessingStatus(Enum):
+    OK = 'OK'
+    FETCH_ERROR = 'FETCH_ERROR'
+    PARSING_ERROR = 'PARSING_ERROR'
+    TIMEOUT = 'TIMEOUT'
+
+    def __str__(self):
+        return str(self.value)
 
 
 async def fetch(session, url, ssl_context):
@@ -32,15 +47,27 @@ async def process_article(
     title,
     results
 ):
-    html = await fetch(
-        session,
-        url,
-        ssl_context=ssl_context,
-    )
-    words = split_by_words(morph, sanitize(html, plaintext=True))
-    rate = calculate_jaundice_rate(words, charged_words)
-    words_count = len(words)
-    results.append((title, rate, words_count))
+    words = None
+    rate = None
+    words_count = None
+    try:
+        async with timeout(1):
+            html = await fetch(
+                session,
+                url,
+                ssl_context=ssl_context,
+            )
+            words = split_by_words(morph, sanitize(html, plaintext=True))
+            rate = calculate_jaundice_rate(words, charged_words)
+            words_count = len(words)
+            status = ProcessingStatus.OK
+    except aiohttp.ClientError:
+        status = ProcessingStatus.FETCH_ERROR
+    except ArticleNotFound:
+        status = ProcessingStatus.PARSING_ERROR
+    except asyncio.TimeoutError:
+        status = ProcessingStatus.TIMEOUT
+    results.append((title, status, rate, words_count))
 
 
 async def main():
@@ -71,8 +98,10 @@ async def main():
                 )
 
         for result in results:
-            title, rate, words_count = result
+            title, status, rate, words_count = result
+            print('')
             print('Заголовок:', title)
+            print(f'Статус: {status}')
             print(f'Рейтинг: {rate}\nКоличество слов: {words_count}')
 
 
